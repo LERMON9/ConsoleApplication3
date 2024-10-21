dd8ad3712cdd3e8ecf3489c681e003ec122fca82
ASUS TUF <ASUS TUF@DESKTOP-T0U8SC9>
2024-10-22 00:12:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <string>  
+// aftours: LeRMoN, Kolia, XeSh, FlasH. school Shag, code: house worken. my tg:https://t.me/LERMONfupa <- click Ctrl+Left mouse, im check sms.Надеюсь правильно написал. \\ 
+using namespace std;
+int main() {
+	cout << "Автомобиль: " << "Audi" << "A4" << ", Год: " << "2002" << ", Двигатель: " << "2.5 TDI AT (163 к.с.)" << ", Пробег: " << "428 тис." << " км" << endl;
+	cout << "Телефон: " << "Iphone" << "13" << ", Память: " << "256" << " ГБ, Диагональ: " << "6.5" << " дюймов, Аккумулятор: " << 2750 << " мАч" << endl;
+	cout << "Книга: " << "ПОРА УЗНАТЬ О ВСЕМ" << ", Автор: " << "Крокодилов Генадий Генадиевич" << ", Страниц: " << "65" << ", Год: " << "1991" << ", Жанр: " << "Исторические романы" << endl;
+}
